b95d9b8b5dfb542a6a2fd881932a68d6600963b9
buptjunjun@gmail.com <buptjunjun@gmail.com@b226407e-f5bd-e256-d17a-cd7466c137c1>
2013-02-17 00:55:16 +0000

    share searchTech

---
+++ trunk/searchTech/src/org/cb/data/BlogField.java
@@ -0,0 +1,10 @@
+package org.cb.data;
+
+public class BlogField
+{
+	static public final String Blog_ID = "id";
+	static public final String Blog_content = "id";
+	static public final String Blog_img = "id";
+	static public final String BlogID = "id";
+	
+}
