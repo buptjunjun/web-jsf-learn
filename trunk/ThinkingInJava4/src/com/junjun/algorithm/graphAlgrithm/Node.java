6d5090a832edb941897a04b45940cf1878fe16f1
buptjunjun@gmail.com <buptjunjun@gmail.com@b226407e-f5bd-e256-d17a-cd7466c137c1>
2013-06-05 06:44:44 +0000

    SSC topologic sort.

--- trunk/ThinkingInJava4/src/com/junjun/algorithm/graphAlgrithm/Node.java
+++ trunk/ThinkingInJava4/src/com/junjun/algorithm/graphAlgrithm/Node.java
@@ -3,7 +3,7 @@ package com.junjun.algorithm.graphAlgrithm;
 import java.util.List;
 
 //节点的数据结构
-public  class Node
+public  class Node implements Comparable
 {
 	//节点名字
 	private String name = "";
@@ -13,6 +13,30 @@ public  class Node
 	
 	private int flag = 0;
 	
+	// 用于拓扑排序
+	private int visitTime = 0;
+	private int beginTime = 0;
+	
+	public int getBeginTime()
+	{
+		return beginTime;
+	}
+
+	public void setBeginTime(int beginTime)
+	{
+		this.beginTime = beginTime;
+	}
+
+	public int getVisitTime()
+	{
+		return visitTime;
+	}
+
+	public void setVisitTime(int visitTime)
+	{
+		this.visitTime = visitTime;
+	}
+
 	public Node(String name)
 	{
 		this.name = name;
@@ -21,10 +45,13 @@ public  class Node
 	@Override
 	public boolean equals(Object obj)
 	{
+		if(obj == null)
+			return false;
+		Node node = (Node)obj;
 		if(name == null)
-			return "null".equals(obj);
+			return  node.name == name;
 		else
-			return name.equals(obj);
+			return name.equals(node.getName());
 	}
 	@Override
 	public int hashCode()
@@ -34,6 +61,17 @@ public  class Node
 			return this.name.hashCode();
 		else
 			return this.hashCode();
+	}
+	
+	@Override
+	public int compareTo(Object arg0)
+	{
+		Node n = (Node)arg0;
+		if(n.getVisitTime() > this.getVisitTime())
+			return 1;
+		else if(n.getVisitTime() < this.getVisitTime())
+			return -1;
+		return 0;
 	}
 	
 	public String getName()
@@ -62,4 +100,5 @@ public  class Node
 		this.flag = flag;
 	}
 
+	
 }
