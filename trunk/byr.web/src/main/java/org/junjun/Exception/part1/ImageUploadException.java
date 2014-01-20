48b9f1684644f668a47162bf59b9e03165e45cd7
buptjunjun@gmail.com <buptjunjun@gmail.com@b226407e-f5bd-e256-d17a-cd7466c137c1>
2014-01-20 06:33:56 +0000

    init web.byr

---
+++ trunk/byr.web/src/main/java/org/junjun/Exception/part1/ImageUploadException.java
@@ -0,0 +1,10 @@
+package org.junjun.Exception.part1;
+
+public class ImageUploadException extends RuntimeException 
+{
+	public ImageUploadException(String msg) {
+		// TODO Auto-generated constructor stub
+		super(msg);
+	}
+	
+}
