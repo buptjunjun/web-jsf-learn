396f2311d6d2a3d96e45afc7c4dc34ed3c5edf30
buptjunjun@gmail.com <buptjunjun@gmail.com@b226407e-f5bd-e256-d17a-cd7466c137c1>
2014-01-20 06:33:56 +0000

    init web.byr

---
+++ byr.web/src/main/java/org/junju/controller/part1/InputController.java
@@ -0,0 +1,28 @@
+package org.junju.controller.part1;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/input")
+public class InputController {
+	 
+	@RequestMapping(value = "/show", method = RequestMethod.GET)
+	 public String showInputPage (Model model ){		
+		
+	         return "inputtest";
+    }
+	@RequestMapping(value = "/inputtest", method = RequestMethod.GET)
+	 public String dealwithInput (@RequestParam("input") String param,String autoparam ,Model model ){
+		 		 String msg = "param:"+param+",autoparam="+autoparam; 
+				  System.out.println();
+		         model.addAttribute("msg", msg);
+	         return "inputtest";
+   }
+}
+
