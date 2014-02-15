111d382139c290f6082825f6adc20b9a828c84c9
buptjunjun@gmail.com <buptjunjun@gmail.com@b226407e-f5bd-e256-d17a-cd7466c137c1>
2014-02-15 09:52:36 +0000

    add solrj

--- trunk/byr.analyzer/src/main/java/byr/analyzer/analyzer/AnalyzerUtil.java
+++ trunk/byr.analyzer/src/main/java/byr/analyzer/analyzer/AnalyzerUtil.java
@@ -3,6 +3,8 @@ package byr.analyzer.analyzer;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.apache.solr.common.SolrInputDocument;
+
 import byr.crawler.framework.Html;
 
 public class AnalyzerUtil {
@@ -22,6 +24,21 @@ public class AnalyzerUtil {
 			+"<field name=\"date\">"+(html.getCreatedDate()==null?sdf.format(new Date()):sdf.format(html.getCreatedDate()))+"/HOUR</field>\n"
 			+"</doc>\n";
 		return ret;
+	}
+	
+	static SolrInputDocument toDoc(HtmlStructuredData html)
+	{
+		SolrInputDocument  doc = new SolrInputDocument();
+	
+		doc.setField("id",html.getId());
+		doc.setField("url",html.getUrl());
+		doc.setField("title",html.getTitle());
+		doc.setField("content",html.getContent());
+		doc.setField("imgs",html.getImgs());
+		doc.setField("videos",html.getVideos());
+		doc.setField("date",(html.getCreatedDate()==null?sdf.format(new Date()):sdf.format(html.getCreatedDate()))+"/HOUR");
+		
+		return doc;
 	}
 	/**
 	 * @param args
--- trunk/byr.analyzer/src/main/java/byr/analyzer/analyzer/Main.java
+++ trunk/byr.analyzer/src/main/java/byr/analyzer/analyzer/Main.java
@@ -2,6 +2,9 @@ package byr.analyzer.analyzer;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -10,6 +13,10 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.common.SolrInputDocument;
 
 import byr.crawler.framework.Html;
 import byr.crawler.persist.DAOMongo;
@@ -25,8 +32,8 @@ public class Main {
 	static String dbname1 = "byr";
 	static HashSet<String> fieldNames = new HashSet<String>();
 	static private Logger logger = Logger.getLogger(Main.class);
-	
-	public static void main(String[] args) 
+	static private SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+	public static void main(String[] args) throws IOException 
 	{
 		fieldNames.add("magicNum");
 		DAOMongo<Html> daoHtml = new DAOMongo<Html>(ip,port,dbname);
@@ -40,7 +47,11 @@ public class Main {
 		
 		ByrHtmlAnalyzer analyzer = new ByrHtmlAnalyzer();
 		List<Html> lh = daoHtml.search(htmlConstrain, 100, Html.class);
-		String ret = "<add>"; 
+		
+		String fileName = sdf.format(new Date())+".xml";
+		List<SolrInputDocument> docs = new ArrayList<SolrInputDocument>();
+		
+		FileUtils.writeStringToFile(new File(fileName), "<add>", "UTF-8", true);
 		while(lh!=null && lh.size() > 0)
 		{
 			for(Html html:lh)
@@ -52,10 +63,12 @@ public class Main {
 					e.printStackTrace();
 				}*/
 				HtmlStructuredData hsd= analyzer.analyse(html);
+			
 				if(hsd != null )
 				{
 					daoStructuredData.insert(hsd);
-					ret+=AnalyzerUtil.toxml(hsd);
+					FileUtils.writeStringToFile(new File(fileName), AnalyzerUtil.toxml(hsd), "UTF-8", true);
+					docs.add(AnalyzerUtil.toDoc(hsd));
 				}
 				
 				logger.info("analyze:"+html.getId()+" "+html.getUrl());
@@ -65,7 +78,7 @@ public class Main {
 				daoHtml.update(html, htmlConstrain1,fieldNames);
 				
 				try {
-					TimeUnit.SECONDS.MILLISECONDS.sleep(100);
+					TimeUnit.SECONDS.MILLISECONDS.sleep(10);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -74,9 +87,31 @@ public class Main {
 			
 			lh = daoHtml.search(htmlConstrain, 100, Html.class);
 		}
+		FileUtils.writeStringToFile(new File(fileName), "</add>", "UTF-8", true);
 		
-		ret+="</add>";
-		System.out.println(ret);
+		indexSolr(docs);
+	}
+	
+	public static void indexSolr(List<SolrInputDocument> docs)
+	{
+		String serverUrl = "http://localhost:8983/solr/collection1";
+		SolrServer solr = new HttpSolrServer(serverUrl);
+		try {
+			solr.add(docs);
+			//hard commit
+			solr.commit(true, false, false);
+		} catch (SolrServerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally
+		{
+			solr.shutdown();
+		}
+		
 	}
 
 }
