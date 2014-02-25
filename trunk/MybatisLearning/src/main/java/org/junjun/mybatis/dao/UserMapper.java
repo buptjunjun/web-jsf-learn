0521d6a12ab2f48ca5f8474800cfc7793a3565ed
buptjunjun@gmail.com <buptjunjun@gmail.com@b226407e-f5bd-e256-d17a-cd7466c137c1>
2014-02-25 10:28:12 +0000

    add insert

--- trunk/MybatisLearning/src/main/java/org/junjun/mybatis/dao/UserMapper.java
+++ trunk/MybatisLearning/src/main/java/org/junjun/mybatis/dao/UserMapper.java
@@ -4,5 +4,8 @@ import org.junjun.mybatis.bean.User;
 
 	public interface UserMapper {  
 	    public User findByName(String name);  
+	    public void insertUser(User user);
+/*	    public void updateUser(User user);
+	    public void deleteUser(String name);*/
 	}  
 
