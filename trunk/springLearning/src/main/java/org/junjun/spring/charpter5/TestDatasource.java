b1b6262ee85548453990f4de1e73d10c6ee332fb
buptjunjun@gmail.com <buptjunjun@gmail.com@b226407e-f5bd-e256-d17a-cd7466c137c1>
2014-03-14 09:57:37 +0000

    spring database configure test OK

---
+++ trunk/springLearning/src/main/java/org/junjun/spring/charpter5/TestDatasource.java
@@ -0,0 +1,35 @@
+package org.junjun.spring.charpter5;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestDatasource
+{
+	public static void main(String [] args)
+	{
+		ApplicationContext app = new ClassPathXmlApplicationContext("charpter5/springdb.xml");
+		DataSource dr = (DataSource) app.getBean("dataSource");
+		
+		try
+		{
+			Connection con = dr.getConnection();
+			PreparedStatement ps = con.prepareStatement("select * from user limit 10");
+			ResultSet rs = ps.executeQuery();
+			while(rs.next())
+			{
+				System.out.println(rs.getString("name"));
+			}
+		} catch (SQLException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
